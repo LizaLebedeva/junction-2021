f77be59e2eebd837e78cd3189bb3b704ae8e11a7
Denis Kovalenko <denis.v.kovalenko@gmail.com>
2021-11-20 10:56:04 +0000

    1pm checkopoint

--- lumen/device.py
+++ lumen/device.py
@@ -4,7 +4,6 @@ class Device:
     def __init__(self, position, device_id):
         self.device_id = device_id
         self.position =position
-        self.events = None
     
     def has_position(self):
         return self.position is not None
@@ -17,9 +16,6 @@ class Device:
         
     def set_device_id(self, device_id):
         self.device_id = device_id
-    
-    def set_events(self, events):
-        self.events = events
 
     def __str__(self):
         return f"Device ID:{self.device_id} at {self.position}"
---
+++ lumen/model_greed.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from scipy.stats.stats import pearsonr
+
+
+class ModelGreed:
+
+    def __init__(self, devices, time_resolution='10s', top_near_size=3):
+        self.devices = devices
+        self.time_resolution = time_resolution
+        self.top_near_size = top_near_size
+        self.series = None
+
+    def fit(self, df_events):
+        
+        df = df_events.copy()
+        df.timestamp = df.timestamp.dt.floor(self.time_resolution)
+        min_time = df.timestamp.min()
+        max_time = df.timestamp.max()
+
+        self.series = pd.DataFrame()
+        self.series['time'] = pd.date_range(start=min_time, end=max_time, freq=self.time_resolution)
+        self.series = self.series.set_index('time')
+        for device_id in sorted(df.deviceid.unique()):
+            _events = list(df[df.deviceid == device_id].timestamp)
+            self.series[device_id] = 0
+            self.series[device_id].loc[_events] = 1
+
+    def get_unknown_devices(self):
+        return [device for device in self.devices if not device.has_device_id()]
+    
+    def get_know_devices(self):
+        return [device for device in self.devices if device.has_device_id()]
+
+    def map_unknowns(self, device_id_candidates):
+        # make a copy
+        device_id_candidates = set([x for x in device_id_candidates])
+        unknown_devices = self.get_unknown_devices()
+        while len(unknown_devices) > 0:
+            device_to_map = unknown_devices[0]
+            ref_position = device_to_map.position
+            # Find know near devices
+            edges_space = []
+            for device in self.get_know_devices():
+                dist = ((device.position[0] - ref_position[0])**2 + (device.position[1] - ref_position[1])**2)**0.5
+                edges_space.append([device, dist])
+            near_devices = sorted(edges_space, key=lambda x:x[1])[:self.top_near_size]        
+    
+            # Find best candidate
+            candidates = []
+            for candidate_device_id in sorted(device_id_candidates):
+                score = 0
+                for near_node in near_devices:
+                    ref_device = near_node[0]
+                    corr = self._calculate_similarity(ref_device.device_id, candidate_device_id)
+                    score += corr
+                candidates.append([candidate_device_id, score])
+            
+            best_id = sorted(candidates, key=lambda x:x[1], reverse=True)[0][0]
+            # Map device
+            device_to_map.set_device_id(best_id)
+            # Remove mapped id from candidates
+            device_id_candidates = device_id_candidates - {best_id}
+            # Update list of unknown
+            unknown_devices = self.get_unknown_devices()
+
+    def _calculate_similarity(self, ref_decide_id, candidate_device_id):
+        return pearsonr(self.series[ref_decide_id].values, self.series[candidate_device_id].values)[0]
